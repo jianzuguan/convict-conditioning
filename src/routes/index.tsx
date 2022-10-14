ea24d90a6d83b959f094580f19bd74bdc73384c9
Jianzu Guan <git@jzg.fastmail.com>
2022-10-14 16:29:09 +0100

    add abs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Chest from './Chest';
+import Abs from './Abs';
 
 export default function AllRoutes() {
   return (
     <Routes>
       <Route path="/chest" element={<Chest />} />
+      <Route path="/abs" element={<Abs />} />
     </Routes>
   );
 }
