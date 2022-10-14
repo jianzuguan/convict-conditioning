34fa0e722f2c0b6945d28fa2a5c4b8758196035d
Jianzu Guan <git@jzg.fastmail.com>
2022-10-14 16:34:31 +0100

    add home

---
+++ src/routes/Home.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+      <p>
+        我就是把囚徒健身里我需要的部分拉出来。
+        方便我自己查找用的。
+      </p>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import Chest from './Chest';
 import Abs from './Abs';
 
 export default function AllRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/chest" element={<Chest />} />
       <Route path="/abs" element={<Abs />} />
     </Routes>
